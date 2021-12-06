6c041682b87818fdc91de5b17c8b40ca90cfc7ae
TheLongPatrol <nolanwangg@gmail.com>
2021-12-05 21:23:49 -0600

    Frontend makes request to server upon text change

--- Project/sentimentanalyzer/src/Utils/GetSentiment.js
+++ Project/sentimentanalyzer/src/Utils/GetSentiment.js
@@ -1,22 +1,32 @@
+import {useState, useEffect} from 'react';
+
 const GetSentiment = (text) => {
-    console.log(text);
-    (async () => {
-        const rawResponse = await fetch('http://localhost:8080/getTone', {
-            method: 'POST',
-            headers: {
-                "Accept": "application/json",
-                "Content-Type": "application/json"
-            },
-            body: {"input": text}
-        });
-        console.log("rawResponse" + rawResponse);
-        const response = await rawResponse.json();
-        console.log("response" + response);
-        const tonesList = response['tones'].map((tone) =>
-            <li> {response['tones']["tone_name"]} </li>);
-        return(<ul>{tonesList}</ul>);
-    })();
+    const [tones, setTones] = useState([]);
+
+    useEffect(() => {
+        async function load() {
+            const rawResponse = await fetch('http://localhost:8080/getTone', {
+                method: 'POST',
+                headers: {
+                    "Accept": "application/json",
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({"input": text})
+            });
+            console.log("rawResponse" + rawResponse);
+            const response = await rawResponse.json();
+            console.log("response" + response);
+            setTones(response['tones']);
+        }
+        load();
+    }, [text])
     
-    
+    return (
+        <ul>
+            {tones.map(tone => (
+                <li>{tone}</li>
+            ))}
+        </ul>
+    )
 }
 export default GetSentiment;
