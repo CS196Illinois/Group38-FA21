768c27e7e2ad0b7e67dfcf2e10f1b33e146c17ce
TheLongPatrol <nolanwangg@gmail.com>
2021-12-16 22:04:44 -0500

    Scrapes 100 most recent tweets given twitter username

--- Project/Server/Server.js
+++ Project/Server/Server.js
@@ -1,52 +1,68 @@
 const express = require('express');
-const path = require('path');
 const cors = require('cors');
+const util = require('util');
+const exec = require('child_process').exec;
+require('dotenv').config();
 const app = express();
+
 const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
 const {IamAuthenticator} = require('ibm-watson/auth');
 const toneAnalyzer = new ToneAnalyzerV3({
     version: '2017-09-21',
     authenticator: new IamAuthenticator({
-        apikey: 'gBM315juVfKWlIiEzn4uNyvhIO3JVy3CKKR8hnWZfMlD'
+        apikey: process.env.IBM_WATSON_KEY,
     }),
-    serviceUrl: 'https://api.us-south.tone-analyzer.watson.cloud.ibm.com/instances/afd81026-a1c6-4e99-a7fb-cf368ae87909',
+    serviceUrl: process.env.IBM_WATSON_URL,
     disableSslVerification: true,
 });
 app.use(express.json());
-app.use(express.static(path.resolve(__dirname, '../sentimentanalyzer/build')));
 app.use(cors());
-app.get('/', function (req, res) {
-  res.sendFile(path.resolve(__dirname, '../sentimentanalyzer/build/index.html'));
-});
-var whitelist = ['http://localhost:3000/', 'http://localhost:3000', 'http://localhost:3000/Analysis']
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
 app.post('/getTone', async (req, res) => {
-  console.log("getting tone" + JSON.stringify(req.body));
-    const toneParams = {
-        toneInput: { 'text': req.body.input.text },
-        contentType: 'text/html',
+  var usernamecommand = 'curl "https://api.twitter.com/2/users/by/username/' + req.body.input.text + '" -H "Authorization: Bearer ' + process.env.TWITTER_BEARER_TOKEN + '"'
+  console.log(usernamecommand);
+  var uid = '';
+  var input = '';
+  child = exec(usernamecommand, function(error, stdout, stderr){
+    uid = JSON.parse(stdout).data.id;
+    var tweetcommand = 'curl "https://api.twitter.com/2/users/' + uid + '/tweets?max_results=100" -H "Authorization: Bearer ' + process.env.TWITTER_BEARER_TOKEN + '"';
+    c = exec(tweetcommand, function(e, stdo, stde){
+      var temp = JSON.parse(stdo).data;
+      for(var i = 0; i < temp.length; i++) {
+        input = input + temp[i].text;
+      }
+      console.log(JSON.parse(stdo).meta);
+      const toneParams = {
+        toneInput: { 'text': input },
+        contentType: 'application/json',
         sentences: false,
-    };
-    console.log(toneParams);
-    const toneAnalysis = await toneAnalyzer.tone(toneParams).catch(err => {
-        console.log('error:', err);
+      };
+      console.log(toneParams);
+      toneAnalyzer.tone(toneParams)
+      .then(toneAnalysis => {
+        const tones = toneAnalysis.result.document_tone.tones;
+        const finalList = [];
+        for (var i = 0; i < tones.length; i++) {
+          finalList.push(tones[i].tone_name);
+        }
+        console.log("finalList" + finalList);
+        res.send({
+          'tones' : finalList
+        })
+      })
+      .catch(err => {
+        console.log(err);
+        res.send({
+          'tones' : []
+        })
+      });
+      
     });
-    const tones = toneAnalysis.result.document_tone.tones;
-    const finalList = [];
-    for (var i = 0; i < tones.length; i++) {
-      finalList.push(tones[i].tone_name);
+    if(error !== null)
+    {
+        console.log('exec error: ' + error);
     }
-    console.log("finalList" + finalList);
-    res.send({
-        'tones' : finalList
-    })
+    
+    });
+    
 })
 app.listen(8080, () => {console.log("Listening on port 8080")});
